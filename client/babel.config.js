8b62598a76a6557bfe030f9c15e53b0b8f4173c9
Eli DeBruyne <eli2.debruyne@gmail.com>
2025-05-01 15:02:45 -0500

    deployment files created and edited

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,5 +2,16 @@ module.exports = function (api) {
     api.cache(true);
     return {
       presets: ['babel-preset-expo', '@babel/preset-typescript'],
+      plugins: [
+        ['module-resolver', {
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/pages',
+            '@utils': './src/utils',
+            '@assets': './assets',
+          },
+        }],
+        'react-native-reanimated/plugin'
+      ],
     };
   };
